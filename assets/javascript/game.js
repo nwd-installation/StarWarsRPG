7a34ce0c3a29d255cac50f5f894ca8e7db811b62
golemheavy <steve@gridtop.com>
2019-03-24 17:52:25 -0500

    incremental

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,35 @@
-var targetNumber = (Math.floor(Math.random() * 102) + 19);
+var imageHero = [];
+var heroes = ["Obi-Wan Kenobi", "Qui-Gon Jinn", "Yoda", "Mace Windu", "Anakin Skywalker", 
+var imageVillain = [];
+var villains = ["Emperor Palpatine", "Darth Maul", "Darth Sidious", "Darth Vader", "Darth Plagueis"] 
 
-$("#number-to-guess").text(targetNumber);
+for (var i = 0; i < heroes.length; i++) {
 
-var counter = 0;
-
-// Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-// We begin by expanding our array to include four options.
-
-var numberOptions = [];
-
-for (i = 0; i < 4 ; i++) {
-	numberOptions[i] = Math.floor(Math.random() * 12) + 1;
+    imageHero[i] = $("<img>");
+    imageHero.addClass("hero-image");
+    imageHero.attr("src", "./assets/images/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+    imageHero.attr("hero-attackvalue", 1);
+    $("#Heros").append(imageHero);
 }
 
-// Next we create a for loop to create crystals for every numberOption.
 
-for (var i = 0; i < numberOptions.length; i++) {
+for (var i = 0; i < villains.length; i++) {
 
-	// For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    imageCrystal.attr("src", "//cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+    imageVillain[i] = $("<img>");
+    imageVillain.addClass("villain-image");
+    imageVillain.attr("src", "./assets/images/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+	imageVillain.attr("villain-attackvalue", 1);
+    $("#Villains").append(imageVillain);
 }
 
-// This time, our click event applies to every single crystal on the page. Not just one.
-$(".crystal-image").on("click", function() {
+$(".hero-image").on("click", function() {
 
     // Determining the crystal's value requires us to extract the value from the data attribute.
     // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
     // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
     // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
 
-    var crystalValue = ($(this).attr("data-crystalvalue"));
+    var heroAttackValue = ($(this).attr("hero-attackvalue"));
    crystalValue = parseInt(crystalValue);
 
     // We then add the crystalValue to the user's "counter" which is a global variable.
