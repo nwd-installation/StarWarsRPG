ae4b9345fe5b55cbe956dad24b8934b34f3447d1
Steve Long <steve@gridtop.com>
2019-03-25 17:34:55 -0500

    this is still not working right but the logic is getting closer to the requirements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,37 @@
-function placeFighter(index) {
+function placeFighter(index, lightSideDarkSide) {
 	if (index < sith.name.length) {
-var imageVillainContainer = $("<div>");
-    imageVillainContainer.addClass("col");
-	$("#sith").append(imageVillainContainer);
+var imageContainer = $("<div>");
+    imageContainer.addClass("col");
+	$("#"+lightSideDarkSide).append(imageContainer);
 
- var imageVillain = $("<img>");
-    imageVillain.addClass("sith-image");
-	imageVillain.attr('id', sith.name[index]);
-    imageVillain.attr("src", sith.imagePath[index]);
-	imageVillain.attr("attack-value", sith.attack[index]);
-	imageVillain.attr("alt", sith.name[index]);
-    $("#sith").append(imageVillain);
+ var imageFighter = $("<img>");
+    imageFighter.addClass(lightSideDarkSide + "-image");
+	imageFighter.attr('id', sith.name[index]);
+    imageFighter.attr("src", sith.imagePath[index]);
+	imageFighter.attr("attack-value", sith.attack[index]);
+	imageFighter.attr("alt", sith.name[index]);
+    $("#"+lightSideDarkSide).append(imageFighter);
 	}	
 
 }
 
+function placeJedi(index, lightSideDarkSide) {
+
+if (index < jedi.name.length) {
+var imageContainer = $("<div>");
+    imageContainer.addClass("col");
+	$("#"+lightSideDarkSide).append(imageContainer);
+
+ var imageFighter = $("<img>");
+    imageFighter.addClass(lightSideDarkSide + "-image");
+	imageFighter.attr('id', jedi.name[index]);
+    imageFighter.attr("src", jedi.imagePath[index]);
+	imageFighter.attr("attack-value", jedi.attack[index]);
+	imageFighter.attr("alt", jedi.name[index]);
+    $("#"+lightSideDarkSide).append(imageFighter);
+	}	
+
+}
 
 var jedi = {
 	
@@ -29,61 +46,15 @@ var sith = {
 	attack	: [1,2,3,4,5]
 };
  
-// for (var i = 0; i < 5; i++) {
-/*
-    var imageHeroContainer = $("<div>");
-    imageHeroContainer.addClass("col");
-	$("#jedi").append(imageHeroContainer);
-	/*
-	var imageHero = $("<img>");
-    imageHero.addClass("jedi-image");
-	imageHero.attr('id', jedi.name[i]);
-    imageHero.attr("src", jedi.imagePath[i]);
-	imageHero.attr("attack-value", jedi.attack[i]);
-	imageHero.attr("alt", jedi.name[i]);
-    $("#jedi").append(imageHero); */
-/* }
-for (var i = 0; i < 5; i++) {
-	
-	var imageVillainContainer = $("<div>");
-    imageVillainContainer.addClass("col");
-	$("#sith").append(imageVillainContainer);
-
-    var imageVillain = $("<img>");
-    imageVillain.addClass("sith-image");
-	imageVillain.attr('id', sith.name[i]);
-    imageVillain.attr("src", sith.imagePath[i]);
-	imageVillain.attr("attack-value", sith.attack[i]);
-	imageVillain.attr("alt", sith.name[i]);
-    $("#sith").append(imageVillain); 
-} */
 
 var gameOver = false;
 var turnCounter = 0;
-var nextTurn = true;
 
 
 $("#next-turn").on("click", function() {
-console.log ("Next Turn Clicked"); turnCounter++; console.log(turnCounter); placeFighter(turnCounter-1);
+console.log ("Next Turn Clicked"); turnCounter++; placeFighter(turnCounter-1, "sith"); placeJedi(turnCounter-1, "jedi"); 
 });
 
-
-
-while (!gameOver && nextTurn) {
-// turnCounter++; console.log(turnCounter);
-nextTurn = false;
-
-var imageHeroContainer = $("<div>");
-    imageHeroContainer.addClass("col");
-	$("#jedi").append(imageHeroContainer);
-
-var imageHero = $("<img>");
-    imageHero.addClass("jedi-image");
-	imageHero.attr('id', jedi.name[turnCounter]);
-    imageHero.attr("src", jedi.imagePath[turnCounter]);
-	imageHero.attr("attack-value", jedi.attack[turnCounter]);
-	imageHero.attr("alt", jedi.name[turnCounter]);
-    $("#jedi").append(imageHero); 
 	
 $(".jedi-image").on("click", function() {
 
@@ -94,24 +65,3 @@ var fighter = {
 console.log ("You have chosen " + fighter.name);
 console.log (fighter.name + " deals " + fighter.attack + " damage.") ;
 });
-
-
-	
-	//pause for input
-	//prompt for attack
-
-/*   
-$(".sith-image").on("click", function() {
-var fighter = {
-	name	:	$(this).attr("alt"),
-	attack	:	$(this).attr("attack-value")
-};
-
-console.log ("You are fighting " + fighter.name);
-console.log (fighter.name + " does " + fighter.attack + " damage.") ;
-
-}); */ 
-
-
-
-}
