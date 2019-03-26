83c30e1b8c1a6bb9303da4e88e31d46c09a73d4a
Steve Long <steve@gridtop.com>
2019-03-26 12:22:44 -0500

    temporarily removing two images, because there are differences between checking a file in the browser on your local, and in checking it on a hosted site. I couldn't get the files to reupload after changing capitalization in the filename. so I will delete them and re-add them.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,79 +1,97 @@
-function placeFighter(index, lightSideDarkSide) {
-	if (index < sith.name.length) {
-/* var imageContainer = $("<div>");
-    imageContainer.addClass("col");
-	$("#"+lightSideDarkSide).append(imageContainer);
-*/ 
- var imageFighter = $("<img>");
-    imageFighter.addClass(lightSideDarkSide + "-image");
-	imageFighter.attr('id', sith.name[index]);
-    imageFighter.attr("src", sith.imagePath[index]);
-	imageFighter.attr("attack-value", sith.attack[index]);
-	imageFighter.attr("alt", sith.name[index]);
-    $("#"+lightSideDarkSide).append(imageFighter);
-	
-	}	
+var opponentLife = 20;
+var playerLife = 20;
 
+function placeFighter(index, lightSideDarkSide) {
+	if (index < sith.name.length) { 
+		var imageFighter = $("<img>");
+		imageFighter.addClass(lightSideDarkSide + "-image");
+		imageFighter.attr('id', sith.name[index]);
+		imageFighter.attr("src", sith.imagePath[index]);
+		imageFighter.attr("attack-value", sith.attack[index]);
+		imageFighter.attr("alt", sith.name[index]);
+		$("#"+lightSideDarkSide).append(imageFighter);
+	}	
 }
 
 function placeJedi(index, lightSideDarkSide) {
-
-if (index < jedi.name.length) {
-/*
-var imageContainer = $("<div>");
-    imageContainer.addClass("col");
-	$("#"+lightSideDarkSide).append(imageContainer);
-*/
- var imageFighter = $("<img>");
-    imageFighter.addClass(lightSideDarkSide + "-image");
-	imageFighter.attr('id', jedi.name[index]);
-    imageFighter.attr("src", jedi.imagePath[index]);
-	imageFighter.attr("attack-value", jedi.attack[index]);
-	imageFighter.attr("alt", jedi.name[index]);
-    $("#"+lightSideDarkSide).append(imageFighter);
+	if (index < jedi.name.length) {
+		var imageFighter = $("<img>");
+		imageFighter.addClass(lightSideDarkSide + "-image");
+		imageFighter.attr('id', jedi.name[index]);
+		imageFighter.attr("src", jedi.imagePath[index]);
+		imageFighter.attr("attack-value", jedi.attack[index]);
+		imageFighter.attr("alt", jedi.name[index]);
+		$("#"+lightSideDarkSide).append(imageFighter);
 	}	
-
 }
 
 var jedi = {
 	
 	name	: ["Obi-Wan Kenobi", "Qui-Gon Jinn", "Mace Windu", "Yoda", "Anakin Skywalker"],
 	imagePath	: ["./assets/images/ObiWanKenobi.jpg","./assets/images/QuiGonJinn.jpg","./assets/images/MaceWindu.jpg","./assets/images/Yoda.jpg","./assets/images/Anakin.jpg"],
-	attack	: [1,2,3,4,5]
+	attack	: [1,2,3,4,5],
+	hp		: [1,2,3,4,5],
+	attackedThisTurn	: [false, false, false, false, false]
 };
 	
 var sith = {
 	name	: ["Darth Maul", "Darth Plagueis", "Chancellor Palpatine", "Darth Vader", "Darth Sidious"],
 	imagePath	: ["./assets/images/DarthMaul.jpeg","./assets/images/DarthPlagueis.jpg","./assets/images/Palpatine.jpg","./assets/images/DarthVader.jpg","./assets/images/DarthSidious.jpeg"],
-	attack	: [1,2,3,4,5]
+	attack	: [1,2,3,4,5],
+	hp		: [1,2,3,4,5],
+	attackedThisTurn	: [false, false, false, false, false]
 };
  
 
 var gameOver = false;
 var turnCounter = 0;
+placeJedi(turnCounter, "jedi"); 
+
 
 
 $("#next-turn").on("click", function() {
-	console.log ("Next Turn Clicked");
-	turnCounter++;
-	placeFighter(turnCounter-1, "sith");
-	placeJedi(turnCounter-1, "jedi"); 
-});
-
-
-	
-$(".jedi-image").on("click", function() {
-
-var fighter = {
-	name	:	$(this).attr("alt"),
-	attack	:	$(this).attr("attack-value")
-};
-console.log ("You have chosen " + fighter.name);
-console.log (fighter.name + " deals " + fighter.attack + " damage.") ;
+	placeFighter(turnCounter, "sith");
+	// opponent will attack if able, or use abilities, before next jedi is placed
+	// need to indicate on screen what phase it is
+	if (turnCounter > 0) {
+		var attacking = [];
+		var attackval = 0;
+		for (var i = 0; i < turnCounter; i++) {
+			attacking.push(sith.name[i]);
+			attackval += sith.attack[i];
+		}
+		console.log("Opponent is attacking with:");
+		console.log(attacking);
+		console.log("For a total of " + attackval + " attack points.");
+		//need to implement a blocking mechanic.
+		console.log("Select fighters to block.");
+		
+		playerLife -= attackval; console.log("Your life points have decreased to " + playerLife);
+		if (playerLife < 1) console.log ("You lose!");
+	}
+	placeJedi(++turnCounter, "jedi");
+	for (var i = 0; i < turnCounter; i++) 
+	{
+		jedi.attackedThisTurn[i] = false;
+		jedi.hp[i] = jedi.attack[i]; //this is meant to restore fighter HP
+	}
 });
 
 document.addEventListener('click', function (event){
-        var clickedValue = event.target.attributes[3];
-        console.log(clickedValue)
-      })
+	var clickedValue = event.target.attributes;
 
+	if (clickedValue[0].value === "jedi-image") {	
+		var fighter = {
+			name	:	clickedValue[1].value,
+			attack	:	clickedValue[3].value
+		};
+		
+		if (turnCounter >= fighter.attack && jedi.hp[fighter.attack] > 0 && !jedi.attackedThisTurn[fighter.attack]) { //i.e., that means that the fighter has been out for at least one turn, and has positive hit points 
+		opponentLife -= fighter.attack;
+		jedi.attackedThisTurn[fighter.attack] = true;
+		console.log (fighter.name + " deals " + fighter.attack + " damage.") ;
+		console.log("Opponent is now at " + opponentLife + " life.");
+		}
+		if (opponentLife < 1) {gameOver = true; console.log("You won!");}
+	}
+});
