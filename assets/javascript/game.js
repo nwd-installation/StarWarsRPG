4c12c82ed5a53cc19b88dfeb6f76f9089ff23ad2
Steve Long <steve@gridtop.com>
2019-03-26 18:28:02 -0500

    placed value trackers in a header row to track round number, whose turn it is, and life points

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,3 @@
-var opponentLife = 20;
-var playerLife = 20;
-// sides = ["sith", "jedi"];
-// side = sides[Math.floor(Math.random * 2)]; // to randomize which side goes first. should also add logic for which side player is playing as
-
-
 //definitions to be used: a turn is all phases of a single player's turn (whether it is player or opponent). A round is a complete round of all players' turns.
 //game should inform player: your turn. Click "Attack to select attackers or click use ability to use a fighter's abilities"
 //note: a fighter may not attack or use abilities until he has been out for one full round, although he can defend ("block")
@@ -17,18 +11,25 @@ var playerLife = 20;
 //only one attack of x fighters per turn
 
 
+var textElements = {
+	turnTrackerText : document.getElementById("turn-tracker"),
+	roundTrackerText : document.getElementById("round-tracker"),
+	playerHPText : document.getElementById("player-health"),
+	opponentHPText : document.getElementById("opponent-health")
+	// notificationText : document.getElementById("notifications")
+};  
+
+// sides = ["sith", "jedi"];
+// side = sides[Math.floor(Math.random * 2)]; // to randomize which side goes first. should also add logic for which side player is playing as
+
+var side = "jedi"; textElements.turnTrackerText.textContent = side;
+var opponentLife = 20; textElements.opponentHPText.textContent = opponentLife;
+var playerLife = 20; textElements.playerHPText.textContent = playerLife;
+var roundCounter = 0; textElements.roundTrackerText.textContent = roundCounter+1;
+
+
 var attackButtonClicked = false;
 var abilityButtonClicked = false;
-
-/*
-var textElements = {
-	turnTrackerText : document.getElementById(""),
-	RoundTrackerText : document.getElementById(""),
-	playerHPText : document.getElementById(""),
-	opponentHPText : document.getElementById(""),
-	notificationText : document.getElementById("")
-};  
-*/
 
 function placeFighter(index, lightSideDarkSide) {
 	if (index < sith.name.length) { 
@@ -89,8 +90,7 @@ var sith = {
  
 
 var gameOver = false;
-var turnCounter = 0;
-placeJedi(turnCounter, "jedi"); 
+placeJedi(roundCounter, "jedi"); 
 
 
 
@@ -104,15 +104,15 @@ function nextTurn() { // add argument parameter: side, to be used in the various
 		var targetDiv = document.getElementById(jedi.name[i]);
 		if (targetDiv) targetDiv.classList.replace("selected-fighter","unready");
 	}
-	placeFighter(turnCounter, "sith");
+	placeFighter(roundCounter, "sith");
 	// opponent will attack if able, or use abilities, before next jedi is placed
 	// need to indicate on screen what phase it is
-	if (turnCounter > 0) {
+	if (roundCounter > 0) {
 		var attacking = [];
 		var attackval = 0;
 		var iteratorLimit;
-		if (turnCounter > sith.name.length) iteratorLimit = sith.name.length; // only iterate up to the number of fighters
-		else iteratorLimit = turnCounter;
+		if (roundCounter > sith.name.length) iteratorLimit = sith.name.length; // only iterate up to the number of fighters
+		else iteratorLimit = roundCounter;
 		for (var i = 0; i < iteratorLimit; i++) {
 			attacking.push(sith.name[i]);
 			attackval += sith.attack[i];
@@ -123,13 +123,14 @@ function nextTurn() { // add argument parameter: side, to be used in the various
 		//need to implement a blocking mechanic.
 		console.log("Select fighters to block.");
 		
-		playerLife -= attackval; console.log("Your life points have decreased to " + playerLife);
+		playerLife -= attackval; console.log("Your life points have decreased to " + playerLife); textElements.playerHPText.textContent = playerLife;
 		if (playerLife < 1) {console.log ("You lose! Game Over"); gameOver = true;}
 	}
 	targetDiv = document.getElementById("next-turn"); targetDiv.classList.replace("inactive-button","control-button2"); 
 	attackButtonClicked = false; targetDiv = document.getElementById("attack-button"); targetDiv.classList.replace("inactive-button","control-button");
 	abilityButtonClicked = false; targetDiv = document.getElementById("ability-button"); targetDiv.classList.replace("inactive-button","control-button");
-	for (var i = 0; i <= turnCounter; i++) 
+	side = "jedi"; textElements.turnTrackerText.textContent = side;
+	for (var i = 0; i <= roundCounter; i++) 
 	{
 		jedi.attackedThisTurn[i] = false;
 		// jedi.hp[i] = jedi.attack[i]; //this is meant to restore fighter HP-- we may want to remove this if we decide that HP don't recover automatically, or recover slowly, etc
@@ -138,7 +139,8 @@ function nextTurn() { // add argument parameter: side, to be used in the various
 		var targetDiv = document.getElementById(jedi.name[i]);
 		if (targetDiv) targetDiv.classList.remove("unready");
 	}
-	placeJedi(++turnCounter, "jedi");
+	placeJedi(++roundCounter, "jedi");
+	textElements.roundTrackerText.textContent = roundCounter+1;
 }
 
 document.addEventListener('click', clickListener);
@@ -148,7 +150,7 @@ function clickListener(event) {
 	var clickedValue = event.target;
 	// console.log(clickedValue.attributes[0]);
 	if (clickedValue.attributes[0]) {
-		if (clickedValue.attributes[0].value === "next-turn") { clickedValue.classList.replace("control-button2","inactive-button"); nextTurn(); }
+		if (clickedValue.attributes[0].value === "next-turn") { clickedValue.classList.replace("control-button2","inactive-button");  side = "sith"; textElements.turnTrackerText.textContent = side; nextTurn(); }
 		else if (clickedValue.attributes[0].value === "attack-button" && abilityButtonClicked === false) {
 			if (attackButtonClicked === false) {
 				attackButtonClicked = true; clickedValue.classList.replace("control-button","inactive-button"); console.log("Select fighter(s) to attack opponent with"); 
@@ -167,10 +169,10 @@ function clickListener(event) {
 			attack	:	clickedValue.attributes[3].value
 		};
 		
-		if (turnCounter >= fighter.attack && jedi.hp[fighter.attack] > 0 && !jedi.attackedThisTurn[fighter.attack]) { //i.e., that means that the fighter has been out for at least one turn, and has positive hit points 
+		if (roundCounter >= fighter.attack && jedi.hp[fighter.attack] > 0 && !jedi.attackedThisTurn[fighter.attack]) { //i.e., that means that the fighter has been out for at least one turn, and has positive hit points 
 		var targetDiv = document.getElementById(fighter.name);
 		targetDiv.setAttribute("class", "jedi-image selected-fighter");
-		opponentLife -= fighter.attack;
+		opponentLife -= fighter.attack; textElements.opponentHPText.textContent = opponentLife;
 		jedi.attackedThisTurn[fighter.attack] = true;
 		console.log (fighter.name + " deals " + fighter.attack + " damage.") ;
 		console.log("Opponent is now at " + opponentLife + " life.");
